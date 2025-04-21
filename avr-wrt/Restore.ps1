c747a0ebba00aff054a4d597cbbf27e9596036fd
Kazuki Okamoto <kazuki.okamoto@kakkun61.com>
2025-04-22 02:11:36 +0900

    Restore すると Free CAD でエラーになってよめないのでやめる

--- avr-wrt/Restore.ps1
+++
@@ -1,4 +0,0 @@
-if (-not (Test-Path -Path .\.bin -PathType Container)) {
-  New-Item -Path .\.bin -ItemType Directory
-}
-Compress-Archive -Path .\avr-wrt.FCStd\* -DestinationPath .\.bin\avr-wrt.FCStd -CompressionLevel NoCompression
--- avr-wrt/Update.ps1
+++
@@ -1,5 +0,0 @@
-if (Test-Path -Path .\avr-wrt.FCStd -PathType Container) {
-  Remove-Item -Path .\avr-wrt.FCStd -Recurse -Force
-}
-New-Item -Path .\avr-wrt.FCStd -ItemType Directory
-Expand-Archive -Path .\.bin\avr-wrt.FCStd -DestinationPath .\avr-wrt.FCStd\
